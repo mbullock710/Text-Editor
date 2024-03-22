a800cb44c8da0eca3cb0dcad0558b23ef0a65582
mbullock710 <michael.bullock@guildeducation.com>
2024-03-22 12:05:58 -0500

    Made updates to database.js file for get and put functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,20 +14,24 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+  // console.error('putDb not implemented');
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store.add({ content });
-  await tx.done;
+  const request = store.put({id: 1, value: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
 }
+
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
+  // console.error('getDb not implemented');
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate','readonly')
   const store = tx.objectStore('jate')
-  return store.getAll();
+  const request = store.get(1);
+  const result = await request;
+  return result?.value;
 }
 
 initdb();
